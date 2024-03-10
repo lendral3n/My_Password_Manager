8b9adc37bc5444e5a01f1f679d42e153fe62bd11
L 3 N <l3noneone@gmail.com>
2024-03-10 08:14:29 +0700

    encrypt password

--- src/password_manager.py
+++ src/password_manager.py
@@ -1,17 +1,20 @@
 from db.database import Database
-
+from .encryption import Encrypt
 class PasswordManager():
     def __init__(self):
         self.dbs = Database()
+        self.encryptor = Encrypt() 
 
     def add_password(self, service, username, password, url, notes):
-        self.dbs.add_password(service, username, password, url, notes)
+        encrypted_password = self.encryptor.encrypt_message(password) 
+        self.dbs.add_password(service, username, encrypted_password, url, notes)
         print(f"Password untuk {service} dan username {username} telah ditambahkan")
         
     def get_password(self, service, username):
-        password = self.dbs.get_password(service, username)
-        if password:
-            print(f"Password untuk {service} dan username {username} adalah {password[0]}")
+        encrypted_password = self.dbs.get_password(service, username)
+        if encrypted_password:
+            password = self.encryptor.decrypt_message(encrypted_password[0])
+            print(f"Password untuk {service} dan username {username} adalah {password}")
         else:
             print(f"Tidak ada password yang ditemukan untuk {service} dan username {username}")
 
